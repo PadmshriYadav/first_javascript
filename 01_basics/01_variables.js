2f4dfecd604a7c96ec34e1593e5fbee6a9bfffc1
PadmshriYadav <yadavpadmshri@gmail.com>
2025-08-16 07:04:37 +0000

    learned varibles

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 12345
+let accountEmail = "yadavpadmshri@gmail.com"
+var accountPassword = "15483" //better use let to delcare variables
+accountCity = "udapur"
+
+
+// account_id = 23
+
+console.log("account_id");
+console.log(accountId);
+
+accountEmail = "kaddu@gmail.com"
+accountPassword = "54623"
+accountCity = "dholakpur"
+let accountState;
+let accountVillage  //works with n witout";"
+
+console.table([accountEmail,accountId,accountCity,accountPassword,accountState,accountVillage]);  
