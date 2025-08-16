8d49bd45842b442af84a8a3717d4c41cd402a3ae
PadmshriYadav <yadavpadmshri@gmail.com>
2025-08-16 07:56:55 +0000

    datatypess

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+
+let age = 18//num
+let name = "anjana"//string
+//null  is object---empty
+//undefined is a type itself..value mot assign yet
+//symbol
+
+console.log(typeof"age")
+
+let student1 = true; //boolean
+console.log("am i dumb?" + student1);
+
+
+console.log(5=="5")
+//true(loose equility checks, not type numb5,string5)
+
+console.log(5==="5");
+//false(strict equlity)
