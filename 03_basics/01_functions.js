a7a43edaf59ffeb355eb352170fe66ba5c8e2f08
PadmshriYadav <yadavpadmshri@gmail.com>
2025-08-19 12:26:57 +0000

    function(object &array)

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -39,5 +39,52 @@ console.log(loginUserMessage())//undefined just logged in
 
 
 //default value function loginUserMessage(username= "sam"){
+
+
+//rest or spred (...)
+
+function calculateCartPrice(num1){
+          return num1
+}
+console.log(calculateCartPrice(2))//2
+console.log(calculateCartPrice(100,2000))//100
+
+function calculateTheCartPrice(...num1){
+          return num1
+}
+console.log(calculateTheCartPrice(100,2000))
+
+function calculateThePrice(var1,var2,...num1){
+          return num1
+}
+console.log(calculateThePrice(100,200,500,800))
+//[ 500, 800 ]...var1=100,var2=200,..num1=[500,800]
+
+
+//.............object in function......
+
+const user = {
+    username: "hitesh",
+    price: 199
+};
+
+function handleObject(anyobject) {
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
 }
 
+// Call the function with the 'user' object
+handleObject(user);
+
+
+
+
+//..................array in function--------------
+
+
+const  myArray =[8,9,78,990,56,63]
+
+function ReturnSecbValue(givenArray){
+    return givenArray[1]
+}
+
+console.log(ReturnSecbValue(myArray))//9
