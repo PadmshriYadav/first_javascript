d0e35723b18e70449c3dfcfe660b896041656eea
PadmshriYadav <yadavpadmshri@gmail.com>
2025-08-27 19:00:13 +0530

    project3

---
+++ 06_projects/proj3/proj3.js
@@ -0,0 +1,10 @@
+
+
+    let clock= document.querySelector("#clock");
+    
+    setInterval( function () {
+        let date = new Date();
+        clock.innerHTML = date.toLocaleTimeString();
+        
+    },1000)
+    
