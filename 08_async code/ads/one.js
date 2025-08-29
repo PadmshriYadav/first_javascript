0aee94f912681a5b013524c9fe16e73568b39a99
PadmshriYadav <yadavpadmshri@gmail.com>
2025-08-29 22:33:38 +0530

    thora improved

--- 08_async code/ads/one.js
+++ 08_async code/ads/one.js
@@ -6,22 +6,44 @@
 
 
 
-const pop = document.querySelector("#pop")
+const button = document.querySelector("#button")
+
 
 const title = document.querySelector("#title")
 
 setInterval(() => {
-
-    if (pop.style.backgroundColor === 'red') {
-        pop.style.backgroundColor = 'blue';    //.style is for incline css
-    } else {
-        pop.style.backgroundColor = 'red';
-    }
     
-    pop.addEventListener("click",(e){
-        
-    })
 
+    if (button.style.backgroundColor === 'red') {
+        button.style.backgroundColor = 'blue';    //.style is for incline css
+    } else {
+        button.style.backgroundColor = 'red';
+    }
 }, 1000); 
 
+closebox();
 
+
+button.addEventListener("click",(e)=>{
+         openbox();
+  });
+
+
+  function openbox(){
+    title.innerHTML = '<br> Engineered for creators with a stunning high-resolution display, powerful processing, and a seamless workflow for editing, design, and rendering.';
+
+    title.innerHTML +='<button id ="close"> close </button>';
+
+    const close=document.getElementById("close");
+      close.addEventListener("click",(e)=>{
+        closebox()
+  });
+
+        
+  };
+
+  function closebox(){
+    title.innerHTML="lets see whats there";
+
+
+  };
