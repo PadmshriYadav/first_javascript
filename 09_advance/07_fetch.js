328751860d0ec41eb30831d279e0d196f5fbe761
PadmshriYadav <yadavpadmshri@gmail.com>
2025-09-06 19:49:42 +0530

    fetch vd xhr

---
+++ 09_advance/07_fetch.js
@@ -0,0 +1,8 @@
+// Fetch is a built-in JavaScript API..promise-based
+//fetch(url).then(response => response.json()).
+
+//----------------------------------------------
+
+
+//XHR is event-based... track the progress and completion of the request.
+// XMLHttpRequest object, open the request, and then send it, all while managing event listeners.
